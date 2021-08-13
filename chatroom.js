31514be5867238751f1f39ed853ede291014d7a7
Brandon Dodd <legendarysandbird@gmail.com>
2021-08-13 12:42:06 -0700

    Finish chat-3: working chat box

--- chatroom.js
+++ chatroom.js
@@ -4,17 +4,26 @@ const fs = require('fs');
 const hostname = '10.0.0.100';
 const port = 8000;
 
-fs.readFile('./index.html', function (err, html) {
-	if (err) throw err;
 
-	http.createServer((req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
+http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	let url = req.url
+	if (url == '/') url += 'index.html'
+	if (url == '/style.css') {
+		res.setHeader('Content-Type', 'text/css');
+	}
+	fs.readFile('./' + url, function (err, html) {
+		if (err) {
+			res.write('404')
+			res.end();
+			return
+		}
 		res.write(html)
 		res.end();
-	}).listen(port);
+	});
+}).listen(port);
 
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log(`Server running at http://${hostname}:${port}/`);
 
 
