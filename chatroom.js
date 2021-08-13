0825a6f5f6ac5d1c75d2e5577f83cae2d304ddff
Brandon Dodd <legendarysandbird@gmail.com>
2021-08-13 12:02:15 -0700

    Fix small typo

--- chatroom.js
+++ chatroom.js
@@ -7,7 +7,7 @@ const port = 8000;
 fs.readFile('./index.html', function (err, html) {
 	if (err) throw err;
 
-	http.createserver((req, res) => {
+	http.createServer((req, res) => {
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html');
 		res.write(html)
