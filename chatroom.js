4558127f324e9e86ef566faf9d5b506aeefc427f
Brandon Dodd <legendarysandbird@gmail.com>
2021-08-13 11:30:50 -0700

    Complete chat-1, no error when loading page

--- chatroom.js
+++ chatroom.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const hostname = '10.0.0.100';
+const port = 8000;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
