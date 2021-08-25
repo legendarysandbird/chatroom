9313f75da465bc8a712860175147eeb9270866f5
Brandon Dodd <legendarysandbird@gmail.com>
2021-08-24 17:03:20 -0700

    Make chat object based instead of pure text/html formatting

--- public/script.js
+++ public/script.js
@@ -7,6 +7,18 @@ while (username == '') {
 	username = prompt("Enter your name: ");
 }
 
+function displayChat(to, items) {
+	// Start with empty slate
+	to.innerHTML = '';
+
+	// Add text one line at a time
+	items.forEach(item => {
+		const line = document.createElement("p");
+		const words = document.createTextNode(item);
+		line.append(words);
+		to.appendChild(line);		
+	});
+}
 
 button.onclick = () => {
 	let time = new Date().toLocaleTimeString('en-US', { hour12: false,
@@ -38,11 +50,8 @@ button.onclick = () => {
 function execute() {
 	fetch("/chat").then(function(response) {
 		response.text().then( (text) => {
-			history.innerHTML = '';
 			let items = JSON.parse(text);
-			items.forEach( item => {
-				history.innerHTML += `${item}<br>`;		
-			});
+			displayChat(history, items);
 		});
 		return "Something";
 	}).then(function(data) {
